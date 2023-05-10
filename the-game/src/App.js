af69e1f8437a9d1661e84b982eca856e1e61c508
tanyapeterson <tanyahallberg@live.se>
2023-05-10 09:44:16 +0200

    small fixes in App

---
+++ the-game/src/App.js
@@ -0,0 +1,16 @@
+import Homepage from "./components/pages/homepage"
+import AnswerButtons from "./components/AnswerButtons";
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+      <AnswerButtons />
+
+
+
+    </div>
+  );
+}
+
+export default App;
